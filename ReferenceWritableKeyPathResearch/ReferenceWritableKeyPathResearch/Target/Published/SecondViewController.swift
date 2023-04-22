e0886a25c13ba50ec05154714d442f7d416f9c5e
kokiTakeda <bluewhitered123@gmail.com>
2023-04-23 00:16:17 +0900

    add research target Published

--- ReferenceWritableKeyPathResearch/ReferenceWritableKeyPathResearch/Target/Published/SecondViewController.swift
+++ ReferenceWritableKeyPathResearch/ReferenceWritableKeyPathResearch/Target/Published/SecondViewController.swift
@@ -4,3 +4,33 @@
 //
 //  Created by 武田孝騎 on 2023/04/14.
 //
+
+import UIKit
+import Combine
+
+class SecondViewController: UIViewController {
+    
+    @IBOutlet private weak var testLabel: UILabel!
+    
+    private let object = SecondTestObject()
+    private var cancels = Set<AnyCancellable>()
+
+    override func viewDidLoad() {
+        self.object.$text.sink { [weak self] value in
+            self?.testLabel.text = value
+        }
+        .store(in: &cancels)
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        // wrappedValueに値を代入するとReferenceWritableKeyPathが生成され、deinitしてもそのReferenceWritableKeyPathは残る。
+        //self.object.text = "test test"
+        // assign(to:)を利用することでReferenceWritableKeyPathを生成せずに値を変更できる。
+        Just("test test").assign(to: &self.object.$text)
+    }
+
+    deinit {
+        print("call deinit‼️")
+    }
+}
