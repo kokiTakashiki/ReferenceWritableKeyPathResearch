46d3aeae9a8c931187fcb2b96dbb4de6ddf8ea4d
kokiTakeda <bluewhitered123@gmail.com>
2023-04-23 00:11:02 +0900

    add: main view controller and other codes

--- ReferenceWritableKeyPathResearch/ReferenceWritableKeyPathResearch/ViewController.swift
+++ ReferenceWritableKeyPathResearch/ReferenceWritableKeyPathResearch/ViewController.swift
@@ -4,3 +4,33 @@
 //
 //  Created by 武田孝騎 on 2023/04/22.
 //
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBAction func transitSecond(_ sender: Any) {
+        let viewController = SecondViewController.instantiateStoryBoardToViewController()
+        self.present(viewController, animated: true)
+    }
+    
+    @IBAction func transitThird(_ sender: Any) {
+        let viewController = ThirdViewController.instantiateStoryBoardToViewController()
+        self.present(viewController, animated: true)
+    }
+
+    @IBAction func transitForth(_ sender: Any) {
+        let viewController = ForthViewController.instantiateStoryBoardToViewController()
+        self.present(viewController, animated: true)
+    }
+    
+    @IBAction func transitFifth(_ sender: Any) {
+        let viewController = FifthViewController.instantiateStoryBoardToViewController()
+        self.present(viewController, animated: true)
+    }
+    
+    @IBAction func transitSixth(_ sender: Any) {
+        let viewController = SixthViewController()
+        self.present(viewController, animated: true)
+    }
+}
+
