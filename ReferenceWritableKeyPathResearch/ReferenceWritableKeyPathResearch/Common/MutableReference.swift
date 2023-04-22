cca8a530d21a9937ce57e71b675ba67c0124aabb
kokiTakeda <bluewhitered123@gmail.com>
2023-04-23 00:12:08 +0900

    add common codes

--- ReferenceWritableKeyPathResearch/ReferenceWritableKeyPathResearch/Common/MutableReference.swift
+++ ReferenceWritableKeyPathResearch/ReferenceWritableKeyPathResearch/Common/MutableReference.swift
@@ -4,3 +4,20 @@
 //
 //  Created by 武田孝騎 on 2023/04/22.
 //
+
+import Foundation
+
+class Reference<Value> {
+    var value: Value
+
+    init(value: Value) {
+        self.value = value
+    }
+}
+
+class MutableReference<Value>: Reference<Value> {
+    func update(with value: Value) {
+        self.value = value
+    }
+}
+
