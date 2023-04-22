8816c77dbf64d0590a4601eb8d8d3d6bdb6e0f79
kokiTakeda <bluewhitered123@gmail.com>
2023-04-23 00:13:55 +0900

    add ui view controller extension

--- ReferenceWritableKeyPathResearch/ReferenceWritableKeyPathResearch/Extensions/UIViewController+Extension.swift
+++ ReferenceWritableKeyPathResearch/ReferenceWritableKeyPathResearch/Extensions/UIViewController+Extension.swift
@@ -4,3 +4,16 @@
 //
 //  Created by 武田孝騎 on 2023/04/22.
 //
+
+import UIKit
+
+extension UIViewController {
+    class func instantiateStoryBoardToViewController<T: UIViewController>() -> T {
+        let storyboardName = String(describing: self.classForCoder())
+        let storyboard = UIStoryboard(name: storyboardName, bundle: nil)
+        guard let viewController = storyboard.instantiateViewController(withIdentifier: storyboardName) as? T else {
+            fatalError("fail create viewController")
+        }
+        return viewController
+    }
+}
